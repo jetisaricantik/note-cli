decae36c7c39e6cdaf1bb6817e63023f6213b6da
jetisaricantik <jetisaricantik051@gmail.com>
2025-10-02 08:23:42 +0200

    Update search.py

--- notecli/search.py
+++ notecli/search.py
@@ -1,1 +1,11 @@
+import os
+from notecli.storage import NOTES_DIR
 
+def search_notes(keyword: str):
+    results = []
+    for file in os.listdir(NOTES_DIR):
+        with open(os.path.join(NOTES_DIR, file), "r", encoding="utf-8") as f:
+            content = f.read()
+            if keyword.lower() in content.lower():
+                results.append((file, content[:80]))
+    return results
